3809b5c40fa432439f80ba67eda2568789ec0a0b
Md Abdullah <155mdabdullah@gmail.com>
2025-08-29 20:16:12 +0600

    Ready To Start Project

---
+++ js/script.js
@@ -0,0 +1,76 @@
+// ==========01. Heart Count Functionality ==========
+
+let heartCount = 0;
+const heartCountNumber = document.getElementById('heart-count');
+const hearts = document.getElementsByClassName('heart');
+
+for (const heart of hearts) {
+    heart.addEventListener('click', function (e) {
+        heartCount++;
+        heartCountNumber.innerText = heartCount;
+
+    });
+}
+
+// ==========02. call Buttons Functionality ==========
+// Shared Function
+// Get Inner text -- Function
+function getinnertext(id) {
+    const innerText = document.getElementById(id).innerText;
+    return innerText;
+}
+
+// Get Inner Text Number -- Function
+function getinnertextNumber(id) {
+    const innerText = parseInt(document.getElementById(id).innerText);
+    return innerText;
+}
+
+// Function to Call Buttons for all Services
+function callBtnFunction(idName, idNumber) {
+    const serviceName = getinnertext(idName);
+    const serviceNumber = getinnertext(idNumber);
+    let availableCoin = getinnertextNumber('available-coin');
+
+    if (availableCoin >= 20) {
+
+        availableCoin -= 20;
+        document.getElementById('available-coin').innerText = availableCoin;
+        alert('📞 Calling ' + serviceName + ' ' + serviceNumber + ' ...')
+    }
+
+    else {
+        alert(" 💰❌ You don't have enough coins. You need at least 20 coins to call.");
+    }
+}
+
+// National Emergency Number
+document.getElementById('national-emergency-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('national-emergency', 'national-emergency-number');
+})
+
+// Police Helpline
+document.getElementById('police-helpline-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('police-helpline', 'police-helpline-number');
+})
+
+// Fire Service Number
+document.getElementById('fire-service-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('fire-service', 'fire-service-number')
+})
+
+// Ambulance Service
+document.getElementById('ambulance-service-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('ambulance-service', 'ambulance-service-number')
+})
+
+// Brac Helpline
+document.getElementById('brac-helpline-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('brac-helpline', 'brac-helpline-number')
+})
+
+// Brac Helpline
+document.getElementById('bangladesh-railway-call-btn').addEventListener('click', function (e) {
+    callBtnFunction('bangladesh-railway', 'bangladesh-railway-number')
+})
+
